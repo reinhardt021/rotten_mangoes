6a429ae969e4d1b0162e6662c36a3f7e27b49063
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-11-24 20:58:20 +0000

    Generated Reviews Controller and New view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RottenMangoes::Application.routes.draw do
-  resources :movies
+  resources :movies do 
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 end
