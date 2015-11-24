79ef506c19d109ce9ddc837f513967f4ddca560b
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-11-24 21:26:19 +0000

    Used some helper methods for reviews average, date formatting, and giving full names

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,23 +4,26 @@ class Movie < ActiveRecord::Base
 
   validates :title,
     presence: true
-
   validates :director,
     presence: true
-
   validates :runtime_in_minutes,
     numericality: { only_integer: true }
-
   validates :description,
     presence: true
-
   validates :poster_image_url,
     presence: true
-
   validates :release_date,
     presence: true
 
   validate :release_date_is_in_the_past
+
+  def review_average
+    if reviews.size > 0
+      reviews.sum(:rating_out_of_ten)/reviews.size 
+    else
+      0
+    end
+  end
 
   protected
 
