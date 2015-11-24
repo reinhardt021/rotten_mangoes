3f6e99c410282ed6f56bac1220dc3227fa4e5f6b
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-11-24 19:52:53 +0000

    Added log in functionality with sessions and auto log in when user created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RottenMangoes::Application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
   resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 end
