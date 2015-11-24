868f268b309db79f0a3859f321a66ed095688f2e
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-11-24 20:05:08 +0000

    Displaying flash notices and alerts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 RottenMangoes::Application.routes.draw do
-  get "sessions/new"
-  get "sessions/create"
   resources :movies
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
