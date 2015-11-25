5a89a86b4b043f912fef2b6dfeda71834e280634
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-11-25 06:54:14 +0000

    Created a redirect for non Admin users, includes flash message

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,12 @@
 class Admin::UsersController < ApplicationController
     
   def index
-    @users = User.all
+    if current_user.admin
+      @users = User.all
+    else
+      flash[:alert] = "You must be an Admin to view the Users page!"
+      redirect_to movies_path
+    end
   end
   
 end
