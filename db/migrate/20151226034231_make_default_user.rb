9453090f4e12b72525c0a48e50da1269f1d6ecf8
reinhardtcgr <reinhardt.cgr@gmail.com>
2015-12-26 05:50:07 +0000

    Updated names to Rotten Mangoed and created default admin user

--- db/migrate/20151226034231_make_default_user.rb
+++ db/migrate/20151226034231_make_default_user.rb
@@ -1,6 +1,8 @@
 class MakeDefaultUser < ActiveRecord::Migration
   def change
-    user = User.new(email: "guest@example.com", firstname: "guest", lastname: "guest", password: "password")
+    user = User.new(email: "guest@example.com", firstname: "Guest", lastname: "Guest", password: "password")
     user.save
+    admin = User.new(email: "admin@example.com", firstname: "Admin", lastname: "Admin", password: "password", admin: true)
+    admin.save
   end
 end
